27fd06b4f95992dbe94aad10653c23e714b07d21
Briyan A <briyana1997@gmail.com>
2022-06-17 15:29:19 -0400

    Correciones

--- api/db.js
+++ api/db.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/ConcerTeckDB`
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
 );
 
-const { Events, Productor, Ticked, Usuario } = sequelize.models
-console.log(sequelize.models)
+const { Events, Productor, Ticked, Usuario } = sequelize.models;
+console.log(sequelize.models);
 
 module.exports = sequelize;
