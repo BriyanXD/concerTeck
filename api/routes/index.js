f8e5b116fea14d18662fc346f78375ac8cb3c15c
solanamr <romerosolana@gmail.com>
2022-06-20 16:28:57 -0300

    feature/ rutas put usuario y productor

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { getUser, createUser,putUser } = require("../controllers/Usuario");
-const {createProductor, getProductor}= require("../controllers/Productor");
+const {createProductor, getProductor, putProductor}= require("../controllers/Productor");
 const {deleteEvent, deleteProductor, deleteUsuario} = require("../controllers/rutas-Delete");
 const routes = Router();
 
@@ -12,6 +12,8 @@ routes.put("/user", putUser);
 
 routes.get("/productor",getProductor);
 routes.post("/productor",createProductor);
+routes.put("/productor", putProductor);
+
 
 routes.delete("/events", deleteEvent);
 routes.delete("/user", deleteUsuario);
