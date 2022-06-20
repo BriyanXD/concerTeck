326be6635262d35b02fd472df9712883d8ca330f
Noeingala <ingalanoelia@gmail.com>
2022-06-20 12:05:25 -0300

    ruteo a home

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-
+import Home from './components/Home/Home.jsx'
 
 function App() {
   return (
     <BrowserRouter>
       <div>
           <Routes>
-            <Route exact path='' element= {<Ruta/>}/>
-            <Route exact path='' element= {<Ruta/>}/>
+            <Route exact path='/' element= {<Home/>}/>
+            {/* <Route exact path='' element= {<Ruta/>}/>
             <Route exact path='' element= {<Ruta/>}/> 
-            <Route exact path='' element= {<Ruta/>}/> 
+            <Route exact path='' element= {<Ruta/>}/>  */}
           </Routes>
       </div>
     </BrowserRouter>
