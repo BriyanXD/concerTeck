a1b59c6bb258e1516a00dab0a545e3a8d347ca1c
arielchauca <69734387+arielchauca@users.noreply.github.com>
2022-06-20 20:10:17 -0300

    Componente Contact + carpeta Modals

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import Contact from '../Contact/Contact';
+import Modal from '../Modals/Modal/Modal.jsx';
 import s from '../Footer/Footer.module.css'
 
 export default function Footer() {
+
+  const [active, setActive] = useState(false);
+
+  const toggle = () => {
+    setActive(!active);
+  }
+
   return (
     <div className={s.containerFooter}>
       <div className={s.info}>
-        {/* column1 */} 
-          <Link  style={{textDecoration:'none'}} to={<Contact/>}>
-          <h4 className={s.letra}>Contactate con Nosotros</h4>
-          </Link>
+        {/* column1 */}
+          <h4 onClick={toggle} className={s.letra}>Contactate con Nosotros</h4>
+          <Modal active={active} toggle={toggle}>
+              <Contact/>
+          </Modal>
         {/* column2 */}
-        <div className={s.redes}> 
+        <div className={s.redes}>
             <a className={s.letra} href="https://www.facebook.com/profile.php?id=100082560332640">Face</a>
             <br />
             <a className={s.letra} href="https://www.instagram.com/concerteck01/">Insta</a>
