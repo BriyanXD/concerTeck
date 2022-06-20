d521f309ad217dd656060b4e1fcc4e6b9bc65de3
arielchauca <69734387+arielchauca@users.noreply.github.com>
2022-06-20 16:40:12 -0300

    Navbar Select Genre y Img logo

--- client/src/components/Filters/Genre/Genre.jsx
+++ client/src/components/Filters/Genre/Genre.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import style from './Genre.module.css';
 
 export default function Genre() {
   return (
-    <div>Genre</div>
+    <div>
+      <select className={style.selectGenre} name="" id="">
+        <option value="">Generos</option>
+      </select>
+    </div>
   )
 }
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -3,13 +3,16 @@ import { Link } from 'react-router-dom';
 import style from './NavBar.module.css';
 import Genre from '../Filters/Genre/Genre';
 import SearchBar from '../SearchBar/SearchBar';
+import logo from '../../assets/concerteck.jpeg'
 
 export default function NavBar() {
   return (
     <div className={style.containerNav}>
       {/* Logitipo que redirecciona a home */}
-      <div className={style.logo}>
-      <Link to="/">Logo</Link>
+      <div className={style.Containerlogo}>
+      <Link to="/">
+        <img className={style.logo} src={logo} alt="Logo not found" />
+      </Link>
       </div>
       <div className={style.containerGenreAndSearch}>
        <Genre/>
