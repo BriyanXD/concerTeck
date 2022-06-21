a3671fc8dd6aad04bfbfa12ce03f59476f85d520
Briyan A <briyana1997@gmail.com>
2022-06-21 10:18:58 -0400

    correcino 2

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,21 +1,10 @@
 const { Router } = require("express");
-<<<<<<< HEAD
-
-const { getUser, createUser, putUser } = require("../controllers/Usuario");
 const {
-  createProductor,
-  getProductor,
-  putProductor,
-} = require("../controllers/Productor");
-const {
-  deleteEvent,
-  deleteProductor,
-  deleteUsuario,
-} = require("../controllers/rutas-Delete");
-const {
-  loadEventsAndGetEvents,
-=======
-const { getUser, createUser, putUser,deleteUser } = require("../controllers/User");
+  getUser,
+  createUser,
+  putUser,
+  deleteUser,
+} = require("../controllers/User");
 const {
   createProducer,
   getProducer,
@@ -27,22 +16,13 @@ const routes = Router();
 const {
   loadEventsAndGetEvents,
   // getEvents,
->>>>>>> fb3183930ba42fa71bca478f2d831a99c4c59166
   postEvents,
   putEvents,
   deleteEvent,
 } = require("../controllers/Events");
 
-<<<<<<< HEAD
-const routes = Router();
-
-routes.get("/user", getUser);
 routes.post("/user", createUser);
 routes.get("/user", getUser);
-=======
-routes.post("/user",createUser);
-routes.get("/user",getUser);
->>>>>>> fb3183930ba42fa71bca478f2d831a99c4c59166
 routes.put("/user", putUser);
 routes.delete("/user", deleteUser);
 
@@ -51,16 +31,10 @@ routes.post("/producer", createProducer);
 routes.put("/producer", putProducer);
 routes.delete("/producer", deleteProducer);
 
-<<<<<<< HEAD
-routes.delete("/events", deleteEvent);
-routes.get("/events", loadEventsAndGetEvents);
-=======
 routes.get("/events", loadEventsAndGetEvents);
 // routes.get("/events:name", getEvents);
->>>>>>> fb3183930ba42fa71bca478f2d831a99c4c59166
 routes.post("/events", postEvents);
 routes.put("/events", putEvents);
 routes.delete("/events", deleteEvent);
 
 module.exports = routes;
-
