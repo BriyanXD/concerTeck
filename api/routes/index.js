13e8b7ee71e53792fa9b188b599b7a3f0b1c2005
Briyan A <briyana1997@gmail.com>
2022-06-20 20:21:58 -0400

    Modificacion: Ruta GET events y Cargado de datos de Eventos a DB. Br

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,22 +1,31 @@
 const { Router } = require("express");
-const { getUser, createUser,putUser } = require("../controllers/Usuario");
-const {createProductor, getProductor, putProductor}= require("../controllers/Productor");
-const {deleteEvent, deleteProductor, deleteUsuario} = require("../controllers/rutas-Delete");
+const { getUser, createUser, putUser } = require("../controllers/Usuario");
+const {
+  createProductor,
+  getProductor,
+  putProductor,
+} = require("../controllers/Productor");
+const {
+  deleteEvent,
+  deleteProductor,
+  deleteUsuario,
+} = require("../controllers/rutas-Delete");
 const routes = Router();
 
+const { loadEventsAndGetAllEvents } = require("../controllers/Events");
 
 routes.get("/user", getUser);
-routes.post("/user",createUser);
-routes.get("/user",getUser);
+routes.post("/user", createUser);
+routes.get("/user", getUser);
 routes.put("/user", putUser);
+routes.delete("/user", deleteUsuario);
 
-routes.get("/productor",getProductor);
-routes.post("/productor",createProductor);
+routes.get("/productor", getProductor);
+routes.post("/productor", createProductor);
 routes.put("/productor", putProductor);
-
+routes.delete("/productor", deleteProductor);
 
 routes.delete("/events", deleteEvent);
-routes.delete("/user", deleteUsuario);
-routes.delete("/productor", deleteProductor);
+routes.get("/events", loadEventsAndGetAllEvents);
 
 module.exports = routes;
