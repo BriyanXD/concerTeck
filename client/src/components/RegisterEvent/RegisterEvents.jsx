2e8907281a63c7daa285dc966082edf017469a90
ToaLukas01 <lucasacua@gmail.com>
2022-06-24 17:51:01 -0300

    formulario de eventos ya funcionando

--- client/src/components/RegisterEvent/RegisterEvents.jsx
+++ client/src/components/RegisterEvent/RegisterEvents.jsx
@@ -14,6 +14,7 @@ import DateTimePicker from 'react-datetime-picker';
 import Footer from '../Footer/Footer';
 import NavBar from '../NavBar/NavBar';
 
+
 export default function RegisterEvent(){
     const dispatch = useDispatch();
     //const [dateTime, setDateTime] = useState(null);
