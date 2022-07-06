2984ef643d6fb5139c2288bc7f6cc6ec865d0af5
Briyan A <briyana1997@gmail.com>
2022-07-06 11:12:08 -0400

    Creacion: Grafica polar, Modificacion: Panel admin Perfil de evento

--- client/src/components/Admin/PerfilEventAdmin.jsx
+++ client/src/components/Admin/PerfilEventAdmin.jsx
@@ -2,6 +2,8 @@ import React from "react"
 import {useSelector, useDispatch} from "react-redux"
 import Style from "./PerfilEventAdmin.module.css"
 import { activeModalEventsAdminPanel } from "../../redux/actions"
+import BarGraph from "../BarGraph/BarGraph"
+import PolarGraph from "../PolarGraph/PolarGraph"
 
 export default function PerfilEventAdmin(){
     const eventSaved = useSelector((state) => state.eventSaved)
@@ -16,12 +18,18 @@ export default function PerfilEventAdmin(){
                 <div className={Style.containerClose}>
                     <button onClick={andlerCloseButton} className={Style.btnClose}>X</button>
                 </div>
+                <p className={Style.name}>{eventSaved.name}</p>
+                <BarGraph event={eventSaved}/>
                 <div className={Style.info}>
-                    <p>ID: {eventSaved.id}</p>
-                    <p>Nombre: {eventSaved.name}</p>
-                    <p>artist: {eventSaved.artist}</p>
-                    <p>schedule: {eventSaved.schedule}</p>
-                    <p>description: {eventSaved.description}</p>
+                   <div className={Style.containerdatediv}>
+                   <div className={Style.containerdate}><span className={Style.dataone}>ID:</span><span>{eventSaved.id}</span></div>
+                    <div className={Style.containerdate}><span className={Style.dataone}>Artist:</span><span>{eventSaved.artist}</span></div>
+                    <div className={Style.containerdate}><span className={Style.dataone}>Schedule:</span><span>{eventSaved.schedule}</span></div>
+                    <div className={Style.containerdate}><span className={Style.dataone}>Description:</span><span>{eventSaved.description}</span></div>
+                   </div>
+                   <div>
+                   <PolarGraph event={eventSaved}/>
+                   </div>
                 </div>
             </div>
         </div>
