7d4d2fbc3ca737a7d9bf0a5b096e112fa7231049
TrosseroAgustin01 <Trosseroagustin01@gmail.com>
2022-07-07 10:15:39 -0300

    Ticket Voucher

--- api/controllers/TicketVoucher.js
+++ api/controllers/TicketVoucher.js
@@ -387,6 +387,7 @@ async function ticketVoucher (req,res){
 
 module.exports={ticketVoucher}
 /*
+console.log()
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
         <html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
         
