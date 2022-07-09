8af2dacc3f8e89d13c14d211c02cfa1fece68518
arielchauca <69734387+arielchauca@users.noreply.github.com>
2022-07-09 17:31:05 -0300

    pasarela ultimos pasos

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import './App.css';
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from './components/Home/Home.jsx';
-import RegisterUser from './components/RegisterUser/RegisterUser.jsx';
-import RegisterEvent from './components/RegisterEvent/RegisterEvents';
-import Detail from './components/Detail/Detail';
-import PageNotFound from './components/PageNotFound/PageNotFound';
-import ProfileUser from './components/ProfileUser/ProfileUser.jsx';
-import PanelAdmin from './components/Admin/adminPanel'
-import Favorites from './components/Favorites/Favorites';
-import TicketVoucher from './components/TicketVoucher/TicketVoucher.jsx';
-import CartCheckout from './components/CartCheckout/CartCheckout';
-import SuccessOrCancel from './components/SuccessOrCancel/SuccessOrCancel.jsx';
-=======
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home.jsx";
@@ -25,31 +10,13 @@ import PanelAdmin from "./components/Admin/adminPanel";
 import Favorites from "./components/Favorites/Favorites";
 import TicketVoucher from "./components/TicketVoucher/TicketVoucher.jsx";
 import Streaming from "./components/Streaming/Streaming.jsx";
->>>>>>> 4dedaf5bee2675c6035fcf20b0db80d3fc88047c
+import SuccessOrCancel from './components/SuccessOrCancel/SuccessOrCancel.jsx';
+import CartCheckout from './components/CartCheckout/CartCheckout.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-<<<<<<< HEAD
-          <Routes>
-            <Route path='/' element= {<Home/>}/>
-            <Route path= '/:id' element={<Detail/>}/>
-            <Route path='/perfil/panelAdmin' element={<PanelAdmin/>}/>
-            <Route exact path='/registrar/:usuario' element= {<RegisterUser/>}/>
-            <Route exact path='/perfil/:id' element={<ProfileUser/>}/>
-            <Route exact path='/ticket/:id' element={<TicketVoucher/>}/>
-            {/* <Route exact path='' element= {<Ruta/>}/> 
-            <Route exact path='' element= {<Ruta/>}/>  */} 
-            <Route exact path='/events' element={<RegisterEvent/>}/>
-            <Route path='/events' element={<RegisterEvent/>}/>
-            <Route path='/favs' element={<Favorites/>}/>
-            <Route path='*' element={<PageNotFound/>}/>
-            <Route path='/pageNotFound' element={<PageNotFound/>}/>
-            <Route path='/cart/checkout' element={<CartCheckout/>}/>
-            <Route path='/success' element={<SuccessOrCancel/>}/>
-          </Routes>
-=======
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/:id" element={<Detail />} />
@@ -64,8 +31,9 @@ function App() {
           <Route path="/events" element={<RegisterEvent />} />
           <Route path="/favs" element={<Favorites />} />
           <Route path="*" element={<PageNotFound />} />
+          <Route path='/success' element={<SuccessOrCancel/>}/>
+          <Route path='/cart/checkout' element={<CartCheckout/>}/>
         </Routes>
->>>>>>> 4dedaf5bee2675c6035fcf20b0db80d3fc88047c
       </div>
     </BrowserRouter>
   );
