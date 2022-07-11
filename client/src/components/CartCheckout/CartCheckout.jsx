2027b440e43f553871a69c9bf98f1ba3b1f13ea6
solanamr <romerosolana@gmail.com>
2022-07-11 19:45:17 -0300

    commit

--- client/src/components/CartCheckout/CartCheckout.jsx
+++ client/src/components/CartCheckout/CartCheckout.jsx
@@ -12,28 +12,42 @@ export default function CartCheckout () {
     const totalTodos = cartDetail.map(item => item.itemTotal).reduce((prev, curr) => prev + curr, 0);
 
     return(
-        <div className={style.container}>
-            <h1 className={style.detalle}>Detalles de la compra</h1>
-            <div>{cartDetail.map((cartD)=>{
-                return(<div>
-                        {/* <img src={cartD.performerImage}/> */}
-                        <div>{cartD.nombre}</div>
-                        <div> {cartD.schedule.split("T")[0]} {'  '}
-                        {cartD.schedule.split("T")[1].split(":")[0] + ":" + cartD.schedule.split("T")[1].split(":")[1]} h</div>
-                        <div> <label>tipo de entrada: {cartD.name} </label> <label> precio:${cartD.price}</label> </div>
-                        <div></div>
-                        <div> <label>cantidad: {cartD.quantity}</label> <label>precio total: ${cartD.itemTotal}</label> </div>
-                        <div> </div>
-                    </div>)
-                })
-            }</div>
+            <div className={style.container}>
+                <h1 className={style.detalle}>Detalles de la compra</h1>
+                <div className={style.card}>
+                    {
+                    cartDetail.map((cartD)=>{
+                    return(
+                        <div className={style.contenedor}>
+                            <img src={cartD.performerImage ? cartD.performerImage : null} className={style.img}/>
+                            <div className={style.cardDetail}>
+                                <div className={style.separacion}>
+                            <div className={style.name}>{cartD.nombre}</div>
+                            <div className={style.schedule}> {cartD.schedule.split("T")[0]} {'  '}
+                            {cartD.schedule.split("T")[1].split(":")[0] + ":" + cartD.schedule.split("T")[1].split(":")[1]} h</div>
+                            <div className={style.type}><label>Tipo de entrada: {cartD.name} </label></div>
+                            </div>
+                            {/* <br /> */}
+                            <div className={style.separacion}>
+                            <div className={style.price}><label>Precio: ${cartD.price}</label> </div>
+                            <div className={style.quantity}> <label>Cantidad: {cartD.quantity}</label></div>
+                            {/* <br /> */}
+                            <div className={style.total}><label>Precio total: ${cartD.itemTotal}</label> </div>
+                            </div>
+                            </div>
+                        </div>
+                        )
+                    })
+                }
+                <div className={style.totalFinal}>Total final: ${totalTodos} ARS.</div>
+                </div>
 
-            <div>Total final: ${totalTodos} ARS.</div>
-
-            <a href={`${url}`}>
-            <button className={style.button}>Siguiente</button>
-            </a> 
-            <div><Link to='/'><button className={style.button}>Volver a inicio</button></Link></div>
-        </div>
-    )
+                <div className={style.btn}>
+                <a href={`${url}`}>
+                <button className={style.button}>Siguiente</button>
+                </a> 
+                <div><Link to='/'><button className={style.button}>Volver a inicio</button></Link></div>
+                </div>
+            </div>
+        )
 }
